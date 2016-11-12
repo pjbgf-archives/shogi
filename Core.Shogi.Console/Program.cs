1ccc8e5599933a1c09689279ab5e0bdd521ebc89
pjbgf <paulo.gomes.uk@gmail.com>
2016-11-12 11:00:57 +0000

    Refactoring unnecessary usings declarations.

--- Core.Shogi.Console/Program.cs
+++ Core.Shogi.Console/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Core.Shogi.Console
+﻿namespace Core.Shogi.Console
 {
     internal class Program
     {
