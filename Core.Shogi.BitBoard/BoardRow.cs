65aed52bd61d9c21d896f07b4dd5c4c8bb8456e5
pjbgf <paulo.gomes.uk@gmail.com>
2016-11-26 10:56:16 +0000

    Created BitRow to represent each row of the board.

---
+++ Core.Shogi.BitBoard/BoardRow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Core.Shogi.BitBoard
+{
+    public struct BoardRow
+    {
+        public static readonly ushort MinValue = 0x0;
+        public static readonly ushort MaxValue = 0x1FF;
+
+        public ushort Value { get; }
+
+        public BoardRow(ushort value)
+        {
+            if (value > MaxValue)
+                throw new ArgumentOutOfRangeException();
+
+            Value = value;
+        }
+    }
+}
---
+++ Core.Shogi.Tests/BitBoard/BoardRowShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Core.Shogi.BitBoard;
+using NUnit.Framework;
+
+namespace Core.Shogi.Tests.BitBoard
+{
+    [TestFixture]
+    [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+    public class BoardRowShould
+    {
+        [Test]
+        public void UseOnlyTwoBytesOfMemory()
+        {
+            var expectedSizeInBytes = 2;
+            var actualSizeInBytes = Marshal.SizeOf<BoardRow>();
+
+            Assert.AreEqual(expectedSizeInBytes, actualSizeInBytes);
+        }
+
+        [Test]
+        public void NotAllowValuesGreaterThan511()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => new BoardRow(0x200) // 512
+            );
+        }
+    }
+}
