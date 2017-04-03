06892646a2902689e028f6ac5a5d695cd9c06530
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-04-03 23:15:55 +0100

    Removing debugging steps.

--- build-test.sh
+++ build-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
 
-# dotnet test Core.Shogi.Tests/Core.Shogi.Tests.csproj
-
 mono ./packages/opencover/4.6.519/tools/OpenCover.Console.exe -target:"/usr/bin/dotnet" -targetargs:"test Core.Shogi.Tests/Core.Shogi.Tests.csproj" -output:coverage.xml -register:user -filter:"+[*]*" -excludebyfile:*Program.cs -oldStyle
+
+exit 0
