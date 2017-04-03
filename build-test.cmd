64f293e2945251ba39ddfcb491e0cb81c75fa632
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-04-03 22:44:22 +0100

    Removing coverage report from GIT. Forcing LS to check whether CI is generating one.

--- build-test.cmd
+++ build-test.cmd
@@ -1,1 +1,1 @@
-@call packages\opencover\4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test Core.Shogi.Tests/Core.Shogi.Tests.csproj" -output:coverage.xml -register:user -filter:"+[*]* -[xunit*]* -[*Program]*" -oldStyle
+@call packages\opencover\4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test Core.Shogi.Tests/Core.Shogi.Tests.csproj" -output:coverage.xml -register:user -filter:"+[*]*" -excludebyfile:*Program.cs -oldStyle
--- build-test.sh
+++ build-test.sh
@@ -2,4 +2,4 @@
 
 # dotnet test Core.Shogi.Tests/Core.Shogi.Tests.csproj
 
-mono ./packages/opencover/4.6.519/tools/OpenCover.Console.exe -target:"/usr/bin/dotnet" -targetargs:"test Core.Shogi.Tests/Core.Shogi.Tests.csproj" -output:coverage.xml -register:user -filter:"+[*]* -[xunit*]* -[*.Program]*" -oldStyle -log:Off
+mono ./packages/opencover/4.6.519/tools/OpenCover.Console.exe -target:"/usr/bin/dotnet" -targetargs:"test Core.Shogi.Tests/Core.Shogi.Tests.csproj" -output:coverage.xml -register:user -filter:"+[*]*" -excludebyfile:*Program.cs -oldStyle
