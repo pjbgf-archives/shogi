4703e3259786cafa2bc7cd4dbc4641c0b49b1b52
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-04-05 20:26:26 +0100

    Removing exit 0 from build-test.

--- build-test.sh
+++ build-test.sh
@@ -5,4 +5,4 @@ rm $HOME/.nuget/packages/opencover/4.6.519/tools/log4net.config
 
 mono $HOME/.nuget/packages/opencover/4.6.519/tools/OpenCover.Console.exe -target:"/usr/bin/dotnet" -targetargs:"test Core.Shogi.Tests/Core.Shogi.Tests.csproj" -output:coverage.xml -register:user -filter:"+[*]*" -excludebyfile:*Program.cs -oldStyle
 
-exit 0
+#exit 0
