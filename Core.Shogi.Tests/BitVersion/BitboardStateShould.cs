b061fe71987468830a8032be8aff1f49b8c7a8e3
Paulo Gomes <Paulo.Gomes.uk@gmail.com>
2018-10-03 23:42:46 +0100

    Add tests for BoardState.

--- Core.Shogi.Tests/BitVersion/BitboardStateShould.cs
+++ Core.Shogi.Tests/BitVersion/BitboardStateShould.cs
@@ -5,7 +5,6 @@ using Xunit;
 
 namespace Core.Shogi.Tests.BitVersion
 {
-    
     public class BitboardStateShould
     {
         [Fact]
---
+++ Core.Shogi.Tests/FullVersion/BoardStateShould.cs
@@ -0,0 +1,37 @@
+﻿using Core.Shogi.FullVersion;
+using Core.Shogi.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Core.Shogi.Tests.FullVersion
+{
+    public class BoardStateShould
+    {
+        [Fact]
+        public void BeAbleToGetExistingPiece()
+        {
+            var expected = new King(Player.Black, "a1");
+            var boardState = new BoardState();
+
+            boardState.Add(expected);
+
+            var actual = boardState.GetPiece("a1");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BeAbleToHandlePieceNotFound()
+        {
+            var boardState = new BoardState();
+
+            boardState.Add(new King(Player.Black, "b2"));
+
+            var actual = boardState.GetPiece("a1");
+
+            Assert.Null(actual);
+        }
+    }
+}
